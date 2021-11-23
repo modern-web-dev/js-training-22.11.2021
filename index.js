707380f0fb96653f4a705639683d950b4ba3fc21
Marek Matczak <marek.matczak79@gmail.com>
2021-11-23 09:44:48 +0100

    objects

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello world!!');
+const person = {
+    'first-name': 'Marek',
+    lastName: 'Matczak',
+    id: 0
+}
+
+// const propName = 'firstName'
+
+// falsy
+// false, null, undefined, 0, '', NaN
+
+person['first-name'] = 'Czarek';
+person.age = 33;
+
+delete person.age;
+
+console.log(person);
