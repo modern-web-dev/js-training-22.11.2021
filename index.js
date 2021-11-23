f0c6fa9b326b9b59d722aa2dcf1480251cbcd46a
Marek Matczak <marek.matczak79@gmail.com>
2021-11-23 11:06:04 +0100

    arrays

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const person = {
-    firstName: 'John',
-    lastName: 'Example',
-    age: 30,
+const myNumbers = [2, 4, 8, 12, 56, 13];
 
-    letMeIntroduceMyself() {
-        return `My name is ${this.firstName} ${this.lastName}`;
-    }
-};
+// let sum = 0;
+// myNumbers.forEach(myNumber => {
+//     const currentNumber = payTax(myNumber);
+//     if (skipFreeTaxValues(currentNumber)) {
+//         sum = calculateSum(sum, currentNumber);
+//     }
+// });
 
-console.log(person.letMeIntroduceMyself());
+// map, filter, reduce
+// shallow copy vs deep copy
+const sum = myNumbers
+    .map(payTax)
+    .filter(skipFreeTaxValues)
+    .reduce(calculateSum, 0);
+
+function payTax(value) {
+    return value - 1;
+}
+
+function skipFreeTaxValues(value) {
+    return value > 4;
+}
+
+function calculateSum (sum, myNumber) {
+    return sum + myNumber;
+}
+
+console.log(sum);
