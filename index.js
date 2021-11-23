82a3ededefc222fa0c453a8b46ba94d0bb0fcea8
Marek Matczak <marek.matczak79@gmail.com>
2021-11-23 11:23:31 +0100

    destructuring / spread

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 const myNumbers = [2, 4, 8, 12, 56, 13];
 
-// let sum = 0;
-// myNumbers.forEach(myNumber => {
-//     const currentNumber = payTax(myNumber);
-//     if (skipFreeTaxValues(currentNumber)) {
-//         sum = calculateSum(sum, currentNumber);
-//     }
-// });
+const copy = [...myNumbers, 23, 56];
 
-// map, filter, reduce
-// shallow copy vs deep copy
-const sum = myNumbers
-    .map(payTax)
-    .filter(skipFreeTaxValues)
-    .reduce(calculateSum, 0);
+const person = {
+    firstName: 'Marek',
+    lastName: 'Matczak',
+    age: 42
+};
 
-function payTax(value) {
-    return value - 1;
+// const personCopy = {
+//     ...person,
+//     firstName: 'Czarek'
+// };
+
+const personCopy = Object.assign(
+    {}, person, {firstName: 'Czarek'});
+
+console.log(sayHelloTo(person));
+
+function sayHelloTo({firstName, ...rest}) {
+    console.log(rest);
+    return `Hello ${firstName}!`
 }
-
-function skipFreeTaxValues(value) {
-    return value > 4;
-}
-
-function calculateSum (sum, myNumber) {
-    return sum + myNumber;
-}
-
-console.log(sum);
