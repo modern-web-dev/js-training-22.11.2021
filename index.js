d354d70ff073df72ce302e88bba3e9856d54461b
Marek Matczak <marek.matczak79@gmail.com>
2021-11-23 13:00:03 +0100

    dom

--- index.js
+++ index.js
@@ -1,24 +1,44 @@
-const myNumbers = [2, 4, 8, 12, 56, 13];
-
-const copy = [...myNumbers, 23, 56];
-
-const person = {
-    firstName: 'Marek',
-    lastName: 'Matczak',
-    age: 42
-};
-
-// const personCopy = {
-//     ...person,
-//     firstName: 'Czarek'
-// };
-
-const personCopy = Object.assign(
-    {}, person, {firstName: 'Czarek'});
-
-console.log(sayHelloTo(person));
-
-function sayHelloTo({firstName, ...rest}) {
-    console.log(rest);
-    return `Hello ${firstName}!`
+class Employee {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
+
+class EmployeeCtrl {
+    constructor() {
+        const employeeForm = document.querySelector('form');
+        this.firstNameInput = employeeForm.querySelector('#firstName');
+        this.lastNameInput = employeeForm.querySelector('#lastName');
+        employeeForm.addEventListener('submit',
+            event => {
+                event.preventDefault();
+                this.saveEmployee();
+            }
+        );
+    }
+
+    // A - apply
+    // B - bind
+    // C - call
+
+    saveEmployee() {
+        console.log(
+            new Employee(this.firstNameInput.value, this.lastNameInput.value));
+    }
+}
+
+const controller = new EmployeeCtrl();
+// controller.firstNameInput.value = 'Marek';
+// controller.saveEmployee();
+
+// data goes down, events go up!
+
+// function add(a, b) {
+//     console.log(this);
+//     return a + b;
+// }
+//
+// add(1, 3);
+// add.call({name: 'dfsfs'}, 1, 3);
+// add.apply({age: 344}, [1, 3]);
